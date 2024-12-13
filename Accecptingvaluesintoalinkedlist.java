378a37a2424b12ce3bc4639de240c23858b6c437
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2024-12-13 15:05:18 +0530

    Add files via upload

---
+++ Accecptingvaluesintoalinkedlist.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Accecptingvaluesintoalinkedlist{
+    public static void main(String[]args){
+        //Accepting values into a linkedlist
+        LinkedList<Integer> a=new LinkedList<>();
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        for(int i=0;i<size;i++){
+            int s=sc.nextInt();
+            a.addFirst(s);
+        }
+        for(int i=0;i<size;i++)
+        System.out.print(a.get(i)+" ");
+    }
+}
