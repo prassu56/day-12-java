4d7744f5e264aad13c643cf950fa44418ca7c742
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2024-12-13 15:22:49 +0530

    Add files via upload

---
+++ Printvaluesofasetusingiterator.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Printvaluesofasetusingiterator{
+    public static void main(String[]args){
+        LinkedHashSet<String> a=new LinkedHashSet<>();
+        a.add("Hello");
+        a.add("Hello");
+        a.add("Students");
+        a.add("Rajesh");
+        a.add("Students");
+        //printing using iterator
+        Iterator itr =a.iterator();
+        while(itr.hasNext())
+        {
+            System.out.print(itr.next()+" ");
+        }   
+        }
+}
